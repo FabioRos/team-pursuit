af4066089d4beabdb519ab5ecf60594abe8eedb5
Fabio Ros <fabio.ros90@gmail.com>
2017-08-07 18:39:27 +0200

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RiderDetailComponent } from './components/rider-detail.component';
 import { ConfigurationComponent } from './components/configuration.component';
+
+
+const appRoutes: Routes = [
+  { path: 'configuration', component: ConfigurationComponent }
+  /*,{ path: '**', component: PageNotFoundComponent }*/
+];
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +24,11 @@ import { ConfigurationComponent } from './components/configuration.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
