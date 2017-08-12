dc666792cb9f3ff94f52350dc5834b09d2e7a277
Fabio Ros <fabio.ros90@gmail.com>
2017-08-12 22:50:48 +0200

    stopwatch stub works

--- src/app/components/tracker.component.ts
+++ src/app/components/tracker.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {NgClass, NgIf, NgFor} from '@angular/common';
+import { StopwatchService} from '../services/stopwatch.service'
+
 
 
 import {Rider} from '../models/rider.model';
@@ -9,14 +12,83 @@ import {RIDERS} from '../mocks/riders.mock';
 @Component({
   selector: 'tracker',
   templateUrl: './tracker.component.html',
-  styleUrls: ['./tracker.component.css']
+  styleUrls: ['./tracker.component.css'],
+  providers: [StopwatchService]
+  
 })
 
 export class TrackerComponent {
-  //selectedRider: Rider;
   riders = RIDERS;
 
-  //onSelect(rider: Rider): void {
-  //  this.selectedRider = rider;
-  //}
+
+  public started: boolean;
+  public stopwatchService: StopwatchService;
+  public time: number;
+
+  private timer: any;
+
+  constructor(stopwatchService: StopwatchService) {
+      this.stopwatchService = stopwatchService;
+      this.time = 0;
+      this.started = false;
+  }
+
+  formatTime(timeMs: number) {
+      let minutes: string,
+          seconds: string;
+
+      minutes = Math.floor(timeMs / 60000).toString();
+      seconds = ((timeMs % 60000) / 1000).toFixed(3);
+      return minutes + ':' + (+seconds < 10 ? '0' : '') + seconds;
+  }
+
+  getUpdate() {
+      let self = this;
+
+      return () => {
+          self.time = this.stopwatchService.time();
+      };
+  }
+
+  lap() {
+      this.update();
+
+      if (this.time) {
+          this.stopwatchService.lap();
+      }
+  }
+
+  reset() {
+      this.stopwatchService.reset();
+      this.started = false;
+      this.update();
+  }
+
+  start() {
+      this.timer = setInterval(this.getUpdate(), 1);
+      this.stopwatchService.start();
+  }
+
+  stop() {
+      clearInterval(this.timer);
+      this.stopwatchService.stop();
+  }
+
+  toggle() {
+      if (this.started) {
+          this.stop();
+      } else {
+          this.start();
+      }
+
+      this.started = !this.started;
+  }
+
+  update() {
+      this.time = this.stopwatchService.time();
+  }
+
+  onClick() {
+      console.log(this.stopwatchService);
+  }
 }
