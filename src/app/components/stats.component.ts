c8465b2662e5bda862565e1c2e8d3251173e6d0b
Fabio Ros <fabio.ros90@gmail.com>
2017-09-07 23:34:23 +0200

    fix chart using time interval c

--- src/app/components/stats.component.ts
+++ src/app/components/stats.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild} from '@angular/core';
 
 import {Rider} from '../models/rider.model';
 import {RIDERS} from '../mocks/riders.mock';
 import {TimeRecord} from '../models/time-record.model'
 import { SimplifiedStopwatchService} from '../services/simplified-stopwatch.service'
+import { ChartComponent } from 'angular2-chartjs';
 
 
 //TODO import chart
@@ -17,41 +18,99 @@ import { SimplifiedStopwatchService} from '../services/simplified-stopwatch.serv
 
 
 
-export class StatsComponent  {
+export class StatsComponent {
   @Input() lastParentEditTimestamp: number;
+  @ViewChild(ChartComponent) chart: ChartComponent; 
   
+  //https://github.com/emn178/angular2-chartjs/blob/master/dev/app.component.ts
   
-  
-  private dataSet: Number[];
-  private labels:  String[];
+  type = 'line';
+  data = {
+    labels: [],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: []
+      }
+    ]
+  };
 
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  
 
   constructor(public stopwatchService: SimplifiedStopwatchService){
     this.reset();
   }
 
-  ngOnChanges(changes) {
-    this.buildDataset();
+  reset(){
+    if(this.stopwatchService.timeRecords!= null && this.stopwatchService.timeRecords.length > 0){
+      this.data = {
+        labels: [],
+        datasets: [
+          {
+            label: "My First dataset",
+            data: []
+          }
+        ]
+      };
+    }
   }
 
- 
-
-
-  reset(){
-    this.dataSet = [];
-    this.labels = [];
-    if(this.stopwatchService.timeRecords!= null && this.stopwatchService.timeRecords.length > 0){
-      this.buildDataset();
-    }
+  refreshAllVariables() {
+    console.log('CHANGE');
+    var data = this.buildDataset();
+    this.refreshChart(data.labels, data.dataset);
   }
 
   buildDataset(){
-    debugger
+    console.log('BUILD DATASET');
     this.reset();
+    var labels = [];
+    var dataset = [];
     for (var i=0; i < this.stopwatchService.timeRecords.length; i++){
-      this.labels[i] = this.stopwatchService.timeRecords[i].rider.toString();
-      this.dataSet[i] = this.stopwatchService.timeRecords[i].lap.time();
+      labels.push(this.stopwatchService.timeRecords[i].rider.toString());
+      dataset[i] = this.stopwatchService.timeRecords[i].lap.time();
     }
+    return {labels: labels, dataset: dataset};
+  }
+
+  refreshChart(labels: String[], dataset: Number[]) {
+    console.log('RC');
+    this.data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "My First dataset",
+          data: dataset
+        }
+      ]
+    };
+   // 
+    
+  }
+
+
+  
+
+
+  //-----------------------------------------------------------------------------------------------------------
+  
+  interval: any;
+  ngOnInit(){
+  this.interval = setInterval(() => {
+       this.refreshAllVariables() ;
+       this.chart.chart.update();
+   }, 5000);
+  }
+
+   ngOnDestroy() {
+   if (this.interval) {
+       clearInterval(this.interval);
+   }
   }
 
 
